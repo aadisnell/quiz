e936ed64c23b6fae2b2f091d2bc8d0d8cfe444e6
MyPc <mypc@infohub.com>
2018-07-12 16:32:34 +0000

    Added the forgotpassword but not yet done

---
+++ online-examination-system/forgotPassword.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'?>
+
+	<title>Fassword activation link Page</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-8 col-md-6 col-lg-10">
+				<div class="card">
+					Provide your Email you used in registering,
+					a link will be sent to it for account reset
+				</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ online-examination-system/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>SSQ HOME || Ricci & Charles Consult </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
